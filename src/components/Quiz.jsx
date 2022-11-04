21a1d2936849e4ca1a82f591a02de410997da32b
aylindoganer <doganeraylinn@gmail.com>
2022-11-04 21:14:55 +0200

    completed rendering answers randomly

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,33 +1,48 @@
 import React from "react"
 import {useState} from "react"
 
-export default function Quiz (props) {
+export default function Quiz ({question, correctAnswer, incorrectAnswers}) {
 
     const [selected, setSelected] = useState(false)
-
-    // const toggleClass = () => {
-    //     setSelected(!selected)
-    // }
-
+    const styles = {
+        backgroundColor: selected ? "#D6DBF5" : ""
+    }
     
+    const [answers, setAnswers] = useState([correctAnswer, ...incorrectAnswers])
+    console.log("answers", answers);
 
+    const toggleClass = () => {
+        setSelected(prevSelected => !prevSelected)
+    }
+
+    let shuffledAnswers = answers
+    .map(value => ({ value, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(({ value }) => value)
+   
     return (
+
         <>
-            <div className="quiz-container">
-                <h1 className="question">{props.question}</h1>
+            <div className="quiz-container" >
+                <h1 className="question">{question}</h1>
                 <div className="answers-container">
-                    <button className={selected ? "selected-answer" : "answers"}>
-                        {props.correctAnswer}
+                    {shuffledAnswers.map((answer, i)=> {
+                        return <button key={i} className="answers" style={styles} onClick={toggleClass}>
+                        {answer}
                     </button>
-                    <button className={selected ? "selected-answer" : "answers"}>
-                        {props.incorrectAnswer}
+                    })}
+                    {/* <button className="answers" style={styles} onClick={toggleClass}>
+                        {correctAnswer}
                     </button>
-                   {Object.keys(props.incorrectAnswer).length > 1 && <button className={selected ? "selected-answer" : "answers"}>
-                        {props.incorrectAnswer}
+                    <button className="answers" style={styles} onClick={toggleClass}>
+                        {incorrectAnswers}
+                    </button>
+                   {Object.keys(incorrectAnswers).length > 1 && <button className="answers" style={styles} onClick={toggleClass}>
+                        {incorrectAnswers}
                     </button>}   
-                    {Object.keys(props.incorrectAnswer).length > 1 && <button className={selected ? "selected-answer" : "answers"}>
-                        {props.incorrectAnswer}
-                    </button>}  
+                    {Object.keys(incorrectAnswers).length > 1 && <button className="answers" style={styles} onClick={toggleClass}>
+                        {incorrectAnswers}
+                    </button>}   */}
                 </div>
             </div>
         </>
